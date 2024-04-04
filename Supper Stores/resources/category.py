431926e554b15007f99e8d1b095a684970d4c212
gabkoi98 <gabakoi18@gmail.com>
2024-04-04 01:14:09 +0000

    This is my flask project

--- Supper Stores/resources/category.py
+++ Supper Stores/resources/category.py
@@ -5,50 +5,46 @@ from db import db
 from flask import jsonify
 from sqlalchemy.exc import SQLAlchemyError
 
-blp = Blueprint("category", __name__, description="Operations on Catetory")
+blp = Blueprint("category", __name__, description="Operations on Category")
 
 from models import CategoryModel
-
 
 @blp.route("/category")
 class Category(MethodView):
     @blp.arguments(CategorySchema)
     @blp.response(200, CategorySchema)
-    def post(self , category_data):
+    def post(self, category_data):
         category = CategoryModel(**category_data)
-        
         try:
             db.session.add(category)
             db.session.commit()
-            
         except SQLAlchemyError:
-              abort(500, message="An error occurred while inserting the category")
-        return jsonify({"Message": "category has been created successfully"}) , 201 
+            abort(500, message="An error occurred while inserting the category")
+        return jsonify({"Message": "category has been created successfully"}), 201
     
+    @blp.response(200, CategorySchema(many=True))
+    def get(self):
+        category = CategoryModel.query.all()
+        
+        
+        return category
     
+        
+@blp.route("/category/<int:category_id>")
+class CategoryInfor(MethodView):
     @blp.response(200, CategorySchema)
-    def delete(self, category_data):
-        category = CategoryModel.query.get_or_404(category_data)
+    def delete(self , category_id):
+        category = CategoryModel.query.get_or_404(category_id)
         
         db.session.delete(category)
         db.session.commit()
         
-        return{"Message": "Category has be delete succcesfuly"}
+        
+        return jsonify({"Message": "category has been deleted successfully"}), 201
     
     
-    @blp.response(200, CategorySchema(many=True))
-    def get(self):
-        category = CategoryModel.query.all(category)
+    @blp.response(200, CategorySchema)
+    def get(self, category_id):
+        category = CategoryModel.query.get_or_404(category_id)
         
         return category
-        
-        
-        
-        
-    
-    
-    
-        
-        
-        
-
